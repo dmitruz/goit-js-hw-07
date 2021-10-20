6d0de257a30324872b4f92f577a63bb703ab7504
dmitruz <dmitruz970@gmail.com>
2021-10-20 21:39:36 +0300

    startt quiz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryImages = document.querySelector('.gallery');
+const cardsImage = createGalleryItemCardsImage(galleryItems);
+let originalImage;
+
+galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+galleryImages.addEventListener('keydown', onClickEscCloseImage);
+
+function createGalleryItemCardsImage(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                 />
+             </a>
+        </div>
+        `;
+        })
+        .join('');
+}
+
+galleryImages.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    originalImage = basicLightbox.create(`
+		<img src="${evt.target.dataset.source}" width="1200" height="900">
+	`)
+    originalImage.show()
+});
+
+
+function onClickEscCloseImage(evt) {
+if (evt.code !== 'Escape') {
+      return;
+    }
+    originalImage.close();
+}
 console.log(galleryItems);
+/
+const galleryImages = document.querySelector('.gallery') 
+    const cardImage = createGalleryItems(galleryItems);
+    cardImage.insertAdjacentHTML('beforeend', cardImage);
+    galleryImages.addEventListener('keydown', onClickEscCloseImage);
+   
+    function createGalleryItems(galleryItems) {
+    return galleryItems.map(({description, original, preview}) => {
+        
+    
+    `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src= ${preview}
+        data-source= ${original}
+        alt=${description}    />
+    </a>
+  </div> `
+  
+})
+.join('');
